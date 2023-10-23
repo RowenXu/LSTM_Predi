58e2aced2bbb22c60834b9aa62e04ab6a6ee2432
rowenxu <Rowenxu>
2023-10-24 00:22:51 +0800

    complete the data processing program

--- data_process.f90
+++ data_process.f90
@@ -1,6 +1,7 @@
-!--------------Data_source----------
-!
+!--------------Data_source--------------
+!SST of Nino34:coming from ERA monthly 
 !
 program data_process
 implcit
+real yy,tt,mm !Time of the initial
 end program
