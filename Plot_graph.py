09aff75e7192efe6422c269281e6be55fc521f28
rowenxu <Rowenxu>
2023-10-26 19:10:58 +0800

    completing the data process and net

--- Plot_graph.py
+++ Plot_graph.py
@@ -3,8 +3,10 @@ import numpy as np #number processing
 import matplotlib.pyplot as plt #to plot
 import cartopy.crs as ccrs
 import cartopy.feature as cfeat
+
 #setting the background
 fig=plt.figure(dpi=100)
 ax=fig.add_subplot(projection=ccrs.PlateCarree())
+
 #input the data 
-plt.show()
+with open("")
